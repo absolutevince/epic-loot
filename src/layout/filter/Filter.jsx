e1d596d596b6364ce1951ba3301c1a8ae973ad55
vince <187113297+absolutevince@users.noreply.github.com>
2025-01-16 10:08:46 +0800

    Optimize layout responsiveness of Home, Shop page and it's children components

--- src/layout/filter/Filter.jsx
+++ src/layout/filter/Filter.jsx
@@ -1,13 +1,48 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../../components/button/Button.jsx";
 import Dropdown from "../../components/drop-down/Dropdown.jsx";
 import style from "./filter.module.css";
 import useStaticData from "../../hooks/useStaticData.jsx";
+import useWindowDimension from "../../hooks/useWindowDimension.jsx";
+
+const breakpoint = 1036;
 
 export default function Filter() {
+  const filtersRef = useRef();
   const genres = useStaticData("genre");
   const platforms = useStaticData("platform");
-  const [filters, setFilters] = useState({ genre: null, platform: null });
+  const stores = useStaticData("store");
+  const publishers = useStaticData("publisher");
+  const tags = useStaticData("tag");
+  const [openFilters, setOpenFilters] = useState(false);
+  const { width, height } = useWindowDimension();
+  const [filters, setFilters] = useState({
+    store: null,
+    genre: null,
+    platform: null,
+    tag: null,
+    publisher: null,
+  });
+
+  useEffect(() => {
+    if (width <= breakpoint) {
+      if (openFilters) {
+        filtersRef.current.style.opacity = 1;
+        filtersRef.current.style.visibility = "visible";
+      } else {
+        filtersRef.current.style.opacity = 0;
+        filtersRef.current.style.visibility = "hidden";
+      }
+    } else {
+      setOpenFilters(false);
+      filtersRef.current.style.opacity = 1;
+      filtersRef.current.style.visibility = "visible";
+    }
+  }, [openFilters, width]);
+
+  const handleOpenFilter = () => {
+    setOpenFilters(!openFilters);
+  };
 
   const setFilterGenre = (value) => {
     setFilters({ ...filters, genre: value });
@@ -16,32 +51,60 @@ export default function Filter() {
   const setFilterPlatform = (value) => {
     setFilters({ ...filters, platform: value });
   };
-  const handleFilter = (value) => {};
+
+  const setFilterStore = (value) => {
+    setFilters({ ...filters, store: value });
+  };
+  const setFilterPublisher = (value) => {
+    setFilters({ ...filters, publisher: value });
+  };
+  const setFilterTag = (value) => {
+    setFilters({ ...filters, tag: value });
+  };
+
+  const handleFilter = () => { };
 
   return (
     <aside className={style.filter}>
-      <Dropdown
-        value={filters.genre}
-        options={genres}
-        name="genre"
-        onDropDownSelect={setFilterGenre}
-      />
-      <Dropdown
-        value={filters.platform}
-        options={platforms}
-        name="platfrom"
-        onDropDownSelect={setFilterPlatform}
-      />
-      <Dropdown
-        value={filters.platform}
-        options={platforms}
-        name="platfrom"
-        onDropDownSelect={setFilterPlatform}
-      />
-      <div className={style.buttons}>
-        <Button onClick={handleFilter} variant="lightExtra">
-          Filter
-        </Button>
+      {width <= breakpoint && (
+        <Button onClick={handleOpenFilter}>Filters</Button>
+      )}
+      <div ref={filtersRef} className={style.list}>
+        <Dropdown
+          value={filters.genre}
+          options={genres}
+          name="genre"
+          onDropDownSelect={setFilterGenre}
+        />
+        <Dropdown
+          value={filters.platform}
+          options={platforms}
+          name="platfrom"
+          onDropDownSelect={setFilterPlatform}
+        />
+        <Dropdown
+          value={filters.store}
+          options={stores}
+          name="store"
+          onDropDownSelect={setFilterStore}
+        />
+        <Dropdown
+          value={filters.publisher}
+          options={publishers}
+          name="publisher"
+          onDropDownSelect={setFilterPublisher}
+        />
+        <Dropdown
+          value={filters.tag}
+          options={tags}
+          name="tag"
+          onDropDownSelect={setFilterTag}
+        />
+        <div className={style.buttons}>
+          <Button onClick={handleFilter} variant="lightExtra">
+            Filter
+          </Button>
+        </div>
       </div>
     </aside>
   );
